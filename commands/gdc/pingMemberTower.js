90d2b3e1e611aab26758fb531291e13340a9db93
PokeStar <pokestar04@gmail.com>
2024-04-25 14:36:32 +0200

    maj

--- commands/gdc/pingMemberTower.js
+++ commands/gdc/pingMemberTower.js
@@ -1,59 +1,59 @@
 const { SlashCommandBuilder } = require("discord.js");
-
+const { Users } = require("../../dbObjects.js");
 const allowedGuildId = '1227242012390985738';
+
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("voler_lego_pour_toujours")
-        .setDescription("Mettre l'heure à laquelle c'est plantations sont finies")
-        .addNumberOption(option =>
-            option.setName('heure')
-                .setDescription('heure')
-                .setRequired(true))
-        .addNumberOption(option =>
-            option.setName('minute')
-                .setDescription('minute')
-                .setRequired(true))
-        .addNumberOption(option =>
-            option.setName('seconde')
-                .setDescription('seconde')
-                .setRequired(true)),
+        .setName("admin_ping_user_gdc")
+        .setDescription("Ping an user by their ID"),
+
     async execute(interaction) {
+
+
         // Vérifier si la commande a été exécutée sur le serveur autorisé
         if (interaction.guildId !== allowedGuildId) {
             return interaction.reply("Désolé, cette commande n'est pas autorisée sur ce serveur.");
         }
 
-        // Récupérer les valeurs des options
-        const heure = interaction.options.getNumber('heure');
-        const minute = interaction.options.getNumber('minute');
-        const seconde = interaction.options.getNumber('seconde');
-
-        // Calculer le temps total en secondes
-        const tempsTotalEnSecondes = heure * 3600 + minute * 60 + seconde - 20;
-        if (tempsTotalEnSecondes < 0) {
-            return interaction.reply("Désolé, la durée spécifiée est trop courte. Veuillez entrer une durée d'au moins 20 secondes.");
-        }
-
-
-        try {
-            // Envoyer le message immédiatement après avoir calculé le temps total
-            await interaction.reply({ content: `Message envoyé dans ${heure} heures, ${minute} minutes et ${seconde} secondes.`, ephemeral: true });
-        } catch (error) {
-            console.error("Une erreur s'est produite lors de l'envoi du message:", error);
-        }
-
-        // Déclencher l'envoi du message après le temps spécifié
-        setTimeout(async () => {
+        if (interaction.member.permissions.has("ADMINISTRATOR")) {
             try {
-                // Pinguer les utilisateurs dans le message
-                const userPing = ['628635503163473929', '503211192970510365'];
-                const mentionedUsers = userPing.map(user => `<@${user}>`).join(' ');
-                await interaction.followUp({ content: `${mentionedUsers}, Go le voler! #MALVEILLANCE MAX`, ephemeral: false });
-            } catch (error) {
-                console.error("Une erreur s'est produite lors de l'envoi du message:", error);
-            }
-        }, tempsTotalEnSecondes * 1000); // setTimeout attend des millisecondes
-    }
+                // Définir les positions à rechercher
+                const positions = ['H', 'M', 'B'];
 
+                // Différer la réponse initiale à l'interaction
+                await interaction.deferReply();
+
+                // Parcourir chaque position
+                for (const position of positions) {
+                    // Récupérer les utilisateurs pour la position actuelle
+                    const usersInPosition = await Users.findAll({
+                        where: { positionTour: position }
+                    });
+                    let positionString = ""
+                    if (position == "H") {
+                        positionString = "Tour du Haut"
+                    }
+                    if (position == "M") {
+                        positionString = "Tour du Millieu"
+                    }
+                    if (position == "B") {
+                        positionString = "Tour du Bas"
+                    }
+                    // Créer un message avec les utilisateurs pour la position actuelle
+                    let message = `Position ${positionString}:\n`;
+                    message += usersInPosition.map(user => `<@${user.dataValues.userId}>`).join(' ');
+
+                    // Envoyer le message
+                    await interaction.followUp(message);
+                }
+            } catch (error) {
+                console.error("An error occurred while executing the admin_ping_user_gdc command:", error);
+                return interaction.reply("An error occurred while executing the command. Please try again later.");
+            }
+        } else {
+            // Si l'utilisateur n'est pas administrateur, répondre avec un message d'erreur
+            return interaction.reply("Désolé, vous devez être administrateur pour exécuter cette commande.");
+        }
+    }
 };
