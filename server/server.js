3c57d3e6d771f5993463fc0f8c80824f6a4452d4
PokeStar <pokestar04@gmail.com>
2024-05-10 23:15:15 +0200

    correftif server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const Sequelize = require('sequelize');
 
 // Créer une application Express
 const app = express();
 
-// Connexion à la base de données SQLite
-const db = new sqlite3.Database('database.sqlite');
+// Connexion à la base de données SQLite avec Sequelize
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+});
+
+// Importer le modèle "users"
+const User = require('../models/users')(sequelize, Sequelize);
 
 // Endpoint pour récupérer les données de la table users
-app.get('/users', (req, res) => {
-    db.all('SELECT * FROM users', (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json(rows);
-    });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 // Port d'écoute de l'API
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://51.254.96.184:${port}`);
+
 });
