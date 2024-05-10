76adcc0373a83b041c84431b73c32c751c36a005
PokeStar <pokestar04@gmail.com>
2024-05-11 01:59:38 +0200

    add update route

--- server/server.js
+++ server/server.js
@@ -24,9 +24,38 @@ app.get('/users', async (req, res) => {
     }
 });
 
+// Endpoint pour mettre à jour les utilisateurs
+app.put('/users', async (req, res) => {
+    const updatedUsers = req.body; // Les utilisateurs mis à jour envoyés depuis votre site internet
+
+    try {
+        // Boucle sur chaque utilisateur mis à jour
+        for (const updatedUser of updatedUsers) {
+            const userId = updatedUser.userId;
+
+            // Vérifier quels champs sont présents dans l'objet utilisateur mis à jour
+            const { pseudo, power, positionTour } = updatedUser;
+            const updateData = {};
+            if (pseudo !== undefined) updateData.pseudo = pseudo;
+            if (power !== undefined) updateData.power = power;
+            if (positionTour !== undefined) updateData.positionTour = positionTour;
+
+            // Effectuer les mises à jour dans la base de données pour chaque utilisateur
+            const user = await User.findByPk(userId);
+            if (user) {
+                await user.update(updateData);
+            }
+        }
+
+        res.status(200).json({ message: 'Users updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 // Port d'écoute de l'API
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://51.254.96.184:${port}`);
-
 });
