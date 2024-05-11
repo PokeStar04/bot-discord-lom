6e3de3069990260e940f8bd5d011bf375d5dd515
PokeStar <pokestar04@gmail.com>
2024-05-11 17:07:24 +0200

    change route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get('/users', async (req, res) => {
     }
 });
 // Endpoint pour mettre à jour les utilisateurs
+// Endpoint pour mettre à jour les utilisateurs
 app.put('/updateUsers', async (req, res) => {
     const updatedUsers = req.body; // Les utilisateurs mis à jour envoyés depuis votre site internet
 
@@ -40,10 +41,39 @@ app.put('/updateUsers', async (req, res) => {
             const updateData = {};
 
             // Vérifiez quels champs sont présents dans l'objet utilisateur mis à jour
-            if (updatedUser.pseudo !== undefined) updateData.pseudo = updatedUser.pseudo;
-            if (updatedUser.power !== undefined) updateData.power = updatedUser.power;
-            if (updatedUser.positionTour !== undefined) updateData.positionTour = updatedUser.positionTour;
             if (updatedUser.ordreTour !== undefined) updateData.ordreTour = updatedUser.ordreTour;
+            if (updatedUser.towerPosition !== undefined) updateData.towerPosition = updatedUser.towerPosition;
+            if (updatedUser.power !== undefined) updateData.power = updatedUser.power;
+
+            // Effectuer les mises à jour dans la base de données pour chaque utilisateur
+            const user = await User.findByPk(userId);
+            if (user) {
+                await user.update(updateData);
+            }
+        }
+
+        res.status(200).json({ message: 'Users updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+// Endpoint pour mettre à jour les utilisateurs
+app.put('/updateUsers', async (req, res) => {
+    const updatedUsers = req.body; // Les utilisateurs mis à jour envoyés depuis votre site internet
+
+    try {
+        // Boucle sur chaque utilisateur mis à jour
+        for (const updatedUser of updatedUsers) {
+            const userId = updatedUser.userId;
+
+            // Créez un objet pour stocker les données de mise à jour
+            const updateData = {};
+
+            // Vérifiez quels champs sont présents dans l'objet utilisateur mis à jour
+            if (updatedUser.ordreTour !== undefined) updateData.ordreTour = updatedUser.ordreTour;
+            if (updatedUser.towerPosition !== undefined) updateData.towerPosition = updatedUser.towerPosition;
+            if (updatedUser.power !== undefined) updateData.power = updatedUser.power;
 
             // Effectuer les mises à jour dans la base de données pour chaque utilisateur
             const user = await User.findByPk(userId);
@@ -58,6 +88,8 @@ app.put('/updateUsers', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error', details: err.message });
     }
 });
+
+
 
 
 
